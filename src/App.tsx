765ba3431c8dee447e7369162290bb8f2dc608db
Yanis HICHER <yhicher@gmail.com>
2025-05-07 11:21:43 +0200

    Intégration i18n avec fr/en

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { useTranslation } from 'react-i18next';
+
 function App() {
+  const { t } = useTranslation();
+
   return (
     <div>
-      <h1>Documento</h1>
-      <p>Site vitrine en cours de développement.</p>
+      <h1>{t('title')}</h1>
+      <p>{t('description')}</p>
     </div>
   );
 }
